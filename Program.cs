09df5977df665ec23fbde43ef303519b04d7d31a
DNabatnikov <nabat008@mail.ru>
2024-02-07 18:47:20 +0700

    добавил условия задачи

--- Program.cs
+++ Program.cs
@@ -43,6 +43,8 @@
 // double sum = i1+i2;
 // System.Console.WriteLine(sum);
 
+// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
 Console.Clear();
 Console.Write("Введите число : ");
 int a = Convert.ToInt32(Console.ReadLine());
